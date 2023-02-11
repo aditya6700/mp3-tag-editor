ecc417b6ee2c74855f31856b5a605e6f9f45c9fa
Sai Aditya Devulapalli <dsaiaditya@gmail.com>
2023-02-11 16:07:29 +0530

    basic code added
    edit mp3 attributes for all songs in a specifed folder
    just basic and hardcoded values

---
+++ mp3Editor.py
@@ -0,0 +1,9 @@
+# load the libraries that we'll use  
+from mutagen.mp3 import MP3  
+from mutagen.easyid3 import EasyID3  
+import mutagen.id3  
+from mutagen.id3 import ID3, TIT2, TIT3, TALB, TPE1, TRCK, TYER  
+  
+import glob  
+  
+import numpy as np  
